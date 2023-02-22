97bba5cca133c0b710f1485440d736a303264945
IVKrylova <ivk.lis@yandex.ru>
2023-02-22 10:44:44 +0300

    feat: add form to create a post

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,30 @@
   <header></header>
   <main class="page">
     <h1 class="h1">Блог</h1>
-      <ul class="postList">
-        <li class="post" v-for="post in posts">
-          <h2 class="post__title">{{ post.title }}</h2>
-          <p class="post__description">{{ post.description }}</p>
-        </li>
-      </ul>
+    <form class="form" @submit.prevent>
+      <h4 class="form__title">Создание поста</h4>
+      <input
+        @input="title = $event.target.value"
+        v-bind:value="title"
+        class="form__input"
+        type="text"
+        placeholder="Название"
+      >
+      <input
+        @input="description = $event.target.value"
+        v-bind:value="description"
+        class="form__input"
+        type="text"
+        placeholder="Описание"
+      >
+      <button class="form__button" @click="createPost">Создать пост</button>
+    </form>
+    <ul class="postList">
+      <li class="post" v-for="post in posts">
+        <h2 class="post__title">{{ post.title }}</h2>
+        <p class="post__description">{{ post.description }}</p>
+      </li>
+    </ul>
   </main>
   <footer></footer>
 </template>
@@ -21,16 +39,22 @@
             { id: 2, title: 'React.js', description: 'React.js - это JavaScript-библиотека с открытым исходным кодом для разработки пользовательских интерфейсов' },
             { id: 3, title: 'Vue.js', description: 'Vue.js — это прогрессивный фреймворк для разработки пользовательских интерфейсов и одностраничных веб-приложений на языке JavaScript' },
           ],
+          title: '',
+          description: '',
         }
       },
       methods: {
-        addLike() {
-          this.likes += 1;
-        },
-        addDislike() {
-          this.dislikes += 1;
-        },
-      }
+        createPost() {
+          const newPost = {
+            id: new Date(),
+            title: this.title,
+            description: this.description,
+          };
+          this.posts.push(newPost);
+          this.title = '';
+          this.description = '';
+        }
+      },
     }
 </script>
 
@@ -69,5 +93,31 @@
     width: 100%;
   }
 
+  .form {
+    display: flex;
+    flex-direction: column;
+    row-gap: 15px;
+    max-width: 500px;
+    Width: 100%;
+    margin: 15px 25px;
+    justify-content: center;
+    align-items: center;
+  }
 
+  .form__input {
+    max-width: 300px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid green;
+  }
+
+  .form__button {
+    box-sizing: border-box;
+    border: none;
+    background-color: green;
+    color: #fff;
+    padding: 10px 5px;
+    cursor: pointer;
+  }
 </style>
