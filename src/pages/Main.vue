207cdaf79255a916d43358fefc140f611e3fc6d5
IVKrylova <ivk.lis@yandex.ru>
2023-02-28 10:29:38 +0300

    style: add style of the main page

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>Блог на Vue.js</h1>
+  <main>
+    <h1 class="h1">Блог на Vue.js</h1>
+  </main>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style>
-
+  .h1 {
+    text-align: center;
+    color: #92A9C9;
+    font-size: 42px;
+    padding: 0;
+    margin: 0;
+  }
 </style>
